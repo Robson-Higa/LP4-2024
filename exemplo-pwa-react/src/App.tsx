aae8ef233b0b9d3412f945c6ec7997b7ac1f711c
Robson-Higa <G38T47ss>
2024-10-15 21:17:16 -0400

    aula 2

--- exemplo-pwa-react/src/App.tsx
+++ exemplo-pwa-react/src/App.tsx
@@ -1,6 +1,6 @@
 import img01 from "./assets/img/img01.jpg";
 import img02 from "./assets/img/img02.jpg";
-import videoSample from "./assets/video/llll.mp4";
+import videoSample from "./assets/video/video.mp4";
 import styles from "./App.module.css";
 
 const App = () => {
@@ -9,16 +9,18 @@ const App = () => {
       <h1 className={styles.title}>Montanhas</h1>
 
       <img src={img01} alt="Uma bela montanha" />
-      <p className={styles.label}>Uma bela Montanha</p>
+      <p className={styles.label}>Uma bela montanha</p>
 
       <img src={img02} alt="Outra bela montanha" />
-      <p className={styles.label}>Outra bela Montanha</p>
+      <p className={styles.label}>Outra bela montanha</p>
 
-      <video autoPlay={true}>
-          <source src={videoSample} type={video/mp4}></source>
+      <video autoPlay={true} loop={true} controls={true}>
+        <source src={videoSample} type="video/mp4" />
       </video>
+      <p className={styles.label}>Um video de uma montanha</p>
+
     </div>
-  )
+  );
 };
 
 export default App;
